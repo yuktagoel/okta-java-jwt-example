432856c45877169527d3885ee70fc13f18bd774b
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-10-29 11:02:30 -0700

    updated secret key

--- src/main/java/com/okta/createverifytokens/JWTDemo.java
+++ src/main/java/com/okta/createverifytokens/JWTDemo.java
@@ -3,8 +3,11 @@ package com.okta.createverifytokens;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
+
 import io.jsonwebtoken.*;
+
 import java.util.Date;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 
@@ -16,7 +19,7 @@ public class JWTDemo {
     // The secret key. This should be in a property file NOT under source
     // control and not hard coded in real life. We're putting it here for
     // simplicity.
-    private static String SECRET_KEY = "ABCD1234";
+    private static String SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
 
     //Sample method to construct a JWT
     public static String createJWT(String id, String issuer, String subject, long ttlMillis) {
@@ -57,5 +60,5 @@ public class JWTDemo {
                 .parseClaimsJws(jwt).getBody();
         return claims;
     }
-    
+
 }
--- src/test/java/com/okta/createverifytokens/JWTDemoTest.java
+++ src/test/java/com/okta/createverifytokens/JWTDemoTest.java
@@ -2,6 +2,7 @@ package com.okta.createverifytokens;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
 import org.junit.Test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,11 +31,11 @@ public class JWTDemoTest {
                 jwtTimeToLive // used to calculate expiration (claim = exp)
         );
         
-        logger.debug("jwt = \"" + jwt.toString() + "\"");
+        logger.info("jwt = \"" + jwt.toString() + "\"");
 
         Claims claims = JWTDemo.decodeJWT(jwt);
 
-        logger.debug("claims = " + claims.toString());
+        logger.info("claims = " + claims.toString());
 
         assertEquals(jwtId, claims.getId());
         assertEquals(jwtIssuer, claims.getIssuer());
@@ -52,6 +53,41 @@ public class JWTDemoTest {
 
         // This will fail with expected exception listed above
         Claims claims = JWTDemo.decodeJWT(notAJwt);
+
+    }
+
+    /*
+    Create a simple JWT, modify it, and try to decode it
+ */
+    @Test(expected = SignatureException.class)
+    public void createAndDecodeTamperedJWT() {
+
+        String jwtId = "SOMEID1234";
+        String jwtIssuer = "JWT Demo";
+        String jwtSubject = "Andrew";
+        int jwtTimeToLive = 800000;
+
+        String jwt = JWTDemo.createJWT(
+                jwtId, // claim = jti
+                jwtIssuer, // claim = iss
+                jwtSubject, // claim = sub
+                jwtTimeToLive // used to calculate expiration (claim = exp)
+        );
+
+        logger.info("jwt = \"" + jwt.toString() + "\"");
+
+        // tamper with the JWT
+
+        StringBuilder tamperedJwt = new StringBuilder(jwt);
+        tamperedJwt.setCharAt(22, 'I');
+
+        logger.info("tamperedJwt = \"" + tamperedJwt.toString() + "\"");
+
+        assertNotEquals(jwt, tamperedJwt);
+
+        // this will fail with a SignatureException
+
+        JWTDemo.decodeJWT(tamperedJwt.toString());
 
     }
 
